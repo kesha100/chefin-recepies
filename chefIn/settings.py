f8051e624e369d6fd137752260d4f67c79ebf9a9
Aruuke <arukeshxo@gmail.com>
2024-05-12 20:17:44 +0600

    render-deploy

--- chefIn/settings.py
+++ chefIn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,17 +77,24 @@ WSGI_APPLICATION = 'chefIn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'chefin_db',
+#         'USER': 'aruuke',
+#         'PASSWORD': 'VGGhZSpnirJqbpHplURbx0gdbBBJI3gd',
+#         'HOST': 'dpg-covptsi1hbls73dv74lg-a.singapore-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chefin_db',
-        'USER': 'aruuke',
-        'PASSWORD': 'VGGhZSpnirJqbpHplURbx0gdbBBJI3gd',
-        'HOST': 'dpg-covptsi1hbls73dv74lg-a.singapore-postgres.render.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('BASE_DIR' , 'db.sqlite3'),
     }
 }
 
+DATABASES["default"] = dj_database_url.parse("postgres://aruuke:VGGhZSpnirJqbpHplURbx0gdbBBJI3gd@dpg-covptsi1hbls73dv74lg-a.singapore-postgres.render.com/chefin_db")
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 

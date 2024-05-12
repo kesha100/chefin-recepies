d69f26640f99ce3671d42d34c5f3906364e190f2
Aruuke <arukeshxo@gmail.com>
2024-05-12 20:51:24 +0600

    render-deploy

--- chefIn/settings.py
+++ chefIn/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c3+e=yy=4u)kxd=ifths0a%&ijiipz-$)kce9qs!!pn$n64bdc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com', '.vercel.app']
 
 
 # Application definition

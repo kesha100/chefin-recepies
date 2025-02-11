6868e7d2d98b07a27af1df2a106ddeace5448ed1
Aruuke <arukeshxo@gmail.com>
2025-02-11 13:17:09 +0600

    vercel config

--- chefIn/settings.py
+++ chefIn/settings.py
@@ -70,9 +70,8 @@ TEMPLATES = [
 ]
 
 
-# WSGI_APPLICATION = 'api.wsgi.app'
-WSGI_APPLICATION = 'chefIn.wsgi.application'
-
+WSGI_APPLICATION = 'api.wsgi.app'
+# WSGI_APPLICATION = 'chefIn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
